b113b6fe203ea637c3b73f5d7ae736a89ad936d3
golangkhoa <145892397+golangkhoa@users.noreply.github.com>
2023-11-20 21:03:14 +0700

    comment lines for c/c++ lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -63,23 +63,23 @@ require("mason-lspconfig").setup_handlers({
 		})
 	end,
 
-	["clangd"] = function()
-		require("neodev").setup()
-		require("lspconfig").clangd.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			cmd = { "clangd" },
-			filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-			single_file_support = true,
-			root_dir = require("lspconfig").util.root_pattern(
-				".clangd",
-				".clang-tidy",
-				".clang-format",
-				"compile_commands.json",
-				"compile_flags.txt",
-				"configure.ac",
-				".git"
-			),
-		})
-	end,
+	-- ["clangd"] = function()
+	-- 	require("neodev").setup()
+	-- 	require("lspconfig").clangd.setup({
+	-- 		capabilities = capabilities,
+	-- 		on_attach = on_attach,
+	-- 		cmd = { "clangd" },
+	-- 		filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+	-- 		single_file_support = true,
+	-- 		root_dir = require("lspconfig").util.root_pattern(
+	-- 			".clangd",
+	-- 			".clang-tidy",
+	-- 			".clang-format",
+	-- 			"compile_commands.json",
+	-- 			"compile_flags.txt",
+	-- 			"configure.ac",
+	-- 			".git"
+	-- 		),
+	-- 	})
+	-- end,
 })
